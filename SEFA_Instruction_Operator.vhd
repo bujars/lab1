cbb16a62c7067eb556ea3e8b9bb31c22942285ee
Bujar Sefa <bujarsefa9@gmail.com>
2021-04-06 11:21:03 -0400

    Add files via upload

--- SEFA_Instruction_Operator.vhd
+++ SEFA_Instruction_Operator.vhd
@@ -16,41 +16,35 @@ end SEFA_Instruction_Operator;
 architecture arch of SEFA_Instruction_Operator is 
 
 SIGNAL SEFA_TEMP_COUT: STD_LOGIC; 
-
+SIGNAL SEFA_AND_RESULT, SEFA_OR_RESULT, SEFA_NOR_RESULT, SEFA_ADD_SIGNED_RESULT, SEFA_SUB_SIGNED_RESULT, SEFA_ADD_UNSIGNED_RESULT, SEFA_SUB_UNSIGNED_RESULT: STD_LOGIC_VECTOR(SEFA_N-1 DOWNTO 0);
 
 begin
 
 --- NOTE, my original attempt was to use if/else statements inside a process. But due to synchronization that was not working (something to loook into)
 --- Now we will use generate as we did in the adder lab (when we want to create components based on a condition). 
+--- NOTE, that using generate that didn't work as well. Becuase it must be static
 
 
-U0: if (SEFA_Operation_Code  = "0000") generate
-	R0:SEFA_Bitwise_AND port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_RD);
-end generate; 
 
-U1: if (SEFA_Operation_Code  = "0001") generate
-	R1:SEFA_Bitwise_OR port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_RD);
-end generate;
 
-U2: if (SEFA_Operation_Code  = "0010") generate
-	R2:SEFA_Bitwise_NOR port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_RD);
-end generate;
 
-U3: if (SEFA_Operation_Code  = "0011") generate 
-	R3: SEFA_ADDER_SUB_SIGNED port map (SEFA_add_sub => '1', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_RD, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
-	end generate;	
+	U0: SEFA_Bitwise_AND port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_AND_RESULT);
+	U1: SEFA_Bitwise_OR port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_OR_RESULT);
+	U2: SEFA_Bitwise_NOR port map (SEFA_RS => SEFA_RS, SEFA_RT => SEFA_RT, SEFA_RD => SEFA_NOR_RESULT);
+	U3: SEFA_ADDER_SUB_SIGNED port map (SEFA_add_sub => '1', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_ADD_SIGNED_RESULT, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
+	U4: SEFA_ADDER_SUB_SIGNED port map (SEFA_add_sub => '0', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_SUB_SIGNED_RESULT, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
+	U5: SEFA_ADDER_SUB_UNSIGNED port map (SEFA_add_sub => '1', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_ADD_UNSIGNED_RESULT, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
+	U6: SEFA_ADDER_SUB_UNSIGNED port map (SEFA_add_sub => '0', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_SUB_UNSIGNED_RESULT, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
 
-U4: if (SEFA_Operation_Code  = "0100") generate 
-	R4: SEFA_ADDER_SUB_SIGNED port map (SEFA_add_sub => '0', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_RD, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
-end generate;				
-			
-U5: if (SEFA_Operation_Code  = "0101") generate 
-	R5: SEFA_ADDER_SUB_UNSIGNED port map (SEFA_add_sub => '1', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_RD, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
-end generate;	
-
-U6: if (SEFA_Operation_Code  = "0110") generate 
-	R6: SEFA_ADDER_SUB_UNSIGNED port map (SEFA_add_sub => '0', SEFA_cin => '0', SEFA_dataa => SEFA_RS, SEFA_datab => SEFA_RT, SEFA_cout => SEFA_TEMP_COUT, SEFA_overflow => SEFA_OVERFLOW, SEFA_result => SEFA_RD, SEFA_negative => SEFA_NEGATIVE, SEFA_ZERO => SEFA_ZERO);
-end generate;
+	SEFA_RD <= SEFA_AND_RESULT WHEN SEFA_Operation_Code = "0000" else
+				SEFA_OR_RESULT WHEN SEFA_Operation_Code = "0001" else
+				SEFA_NOR_RESULT WHEN SEFA_Operation_Code = "0010" else
+				SEFA_ADD_SIGNED_RESULT WHEN SEFA_Operation_Code = "0011" else
+				SEFA_SUB_SIGNED_RESULT WHEN SEFA_Operation_Code = "0100" else
+				SEFA_ADD_UNSIGNED_RESULT WHEN SEFA_Operation_Code = "0101" else
+				SEFA_SUB_UNSIGNED_RESULT;
+				
+					
 
 	
 
